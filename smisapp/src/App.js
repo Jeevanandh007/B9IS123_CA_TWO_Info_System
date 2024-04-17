4d6f1da37fcc066d55c1284dea2ee5a3571aa9cc
aleenaros7 <aleenaros777@gmail.com>
2024-04-17 15:11:36 +0100

    appjs

--- smisapp/src/App.js
+++ smisapp/src/App.js
@@ -1,9 +1,18 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Navbar, Nav, Container } from 'react-bootstrap'; 
 
 function App() {
   return (
     <div className="App">
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="#">
+            <img src={logo} alt="Logo" width="30" height="30" className="d-inline-block align-top"/>
+            <Navbar.Brand/>
+          <Container/>
+          <Navbar/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
