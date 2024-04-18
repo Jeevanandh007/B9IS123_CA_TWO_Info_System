f76dba82ddb9ce8a6190aa8ab828703b298fb3c2
Jeevanandh <jeevavijay24@gmail.com>
2024-04-18 05:20:53 +0100

    db created

--- SMIS/SMIS/settings.py
+++ SMIS/SMIS/settings.py
@@ -75,9 +75,13 @@ WSGI_APPLICATION = "SMIS.wsgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'create',
+        'USER': 'postgres',
+        'PASSWORD': 'password123',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
