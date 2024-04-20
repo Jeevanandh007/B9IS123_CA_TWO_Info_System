39e4027624f1cee7ca1c71ff694ec101681eaa5c
Jeevanandh <jeevavijay24@gmail.com>
2024-04-20 16:48:04 +0100

    user table and product table added

--- SMIS/SMIS/settings.py
+++ SMIS/SMIS/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'customers',
+    'logistics',
+    'user_login',
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
--- SMIS/user_login/models.py
+++ SMIS/user_login/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class logisticsusers(models.Model):
+    USER_ID=models.AutoField(primary_key=True)
+    USER_NAME=models.CharField(max_length=150)
+    PASSWORD=models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.USER_NAME
