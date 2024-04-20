d6443c5216edd0d79a57224476da6a8721cfe87f
Jeevanandh <jeevavijay24@gmail.com>
2024-04-20 16:22:06 +0100

    model update

--- SMIS/customers/models.py
+++ SMIS/customers/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-class orderdetails(models.):
+class orderdetails(models.Model):
     ORDER_ID=models.AutoField(primary_key=True)
     CUSTOMER_NAME=models.CharField(max_length=150)
     EMAIL=models.EmailField()
@@ -12,7 +12,7 @@ class orderdetails(models.):
     ADDRESS=models.TextField()
     PINCODE=models.CharField(max_length=10)
     COUNTRY=models.CharField(max_length=50)
-    ORDERE_STATUS=models.CharField(max_length=50,default='Pending')
+    ORDER_STATUS=models.CharField(max_length=50,default='Pending')
     ORDER_DATE=models.DateField(auto_now_add=True)
     ESTIMATED_DATE_OF_DELIVERY=models.DateField()
     DELIVERY_METHOD = models.CharField(max_length=50)
