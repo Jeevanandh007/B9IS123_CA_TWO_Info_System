5fd5b5dc2b6750c0fc9f2ca12582059b99048452
Jeevanandh <jeevavijay24@gmail.com>
2024-04-20 15:54:05 +0100

    create model for database

--- SMIS/customers/models.py
+++ SMIS/customers/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class order_details(models.):
+    CUSTOMER_NAME=models.CharField(max_length=150)
+    EMAIL=models.EmailField()
+    PHONE_NUMBER=models.CharField(max_length=10)
+    PRODUCT=models.CharField(max_length=50)
+    DOSAGE=models.IntegerField()
+    ADDRESS=models.TextField()
+    PINCODE=models
