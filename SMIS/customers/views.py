aad50ac7f4527634c507355e46a1befc5ee769f2
TijuThampan <tijuthampan96@gmail.com>
2024-04-21 02:12:52 +0100

    Views for handling API requests in customers/views.py

--- SMIS/customers/views.py
+++ SMIS/customers/views.py
@@ -1,6 +1,14 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView
+# from django.shortcuts import render
+# from django.views.generic import TemplateView
 
-# Create your views here.
-class Index(TemplateView):
-    template_name ='customers/index.html'
+# # Create your views here.
+# class Index(TemplateView):
+#     template_name ='customers/index.html'
+
+from rest_framework import generics
+from .models import OrderDetails
+from .serializers import OrderDetailsSerializer
+
+class OrderDetailsListCreate(generics.ListCreateAPIView):
+    queryset = OrderDetails.objects.all()
+    serializer_class = OrderDetailsSerializer
