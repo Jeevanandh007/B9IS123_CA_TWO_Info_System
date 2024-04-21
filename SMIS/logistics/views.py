f07c3186908e98647eb724ae62e0e2a405f4832f
Jeevanandh <jeevavijay24@gmail.com>
2024-04-21 12:49:02 +0100

    Created view for product and dosage update api

--- SMIS/logistics/views.py
+++ SMIS/logistics/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import updateproductserializer
+from .models import products
 
 # Create your views here.
+
+class updateproductsview(viewsets.ModelViewSet):
+    serializer_class =updateproductserializer
+    queryset = products.objects.all()
