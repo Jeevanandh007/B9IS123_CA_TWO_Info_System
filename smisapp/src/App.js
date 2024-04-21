bc09aac48b206492a23bdd6ff1fd34549a5b5ff3
Jeevanandh <jeevavijay24@gmail.com>
2024-04-21 21:33:39 +0100

    frontend update

--- smisapp/src/App.js
+++ smisapp/src/App.js
@@ -1,43 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { Navbar, Nav, Container, Form, FormLabel, FormControl, Button, Row, Col } from 'react-bootstrap';
+
+import { Nav, Container, Form, FormLabel, FormControl, Button, Row, Col } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
-      <Navbar bg="light" expand="lg" className="Navbar">
-        <header_Container>
-          <Navbar.Brand href="#" className="Navbar-brand">
-            <img src={logo} alt="Logo" width="30" height="30" className="d-inline-block align-top" />
-            Sales Management System
-          </Navbar.Brand>
 
-          {/*toggle button*/}
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <div className="header_title">
+        
+        <Nav href="#" className="Project">
+            Sales Management Information System
+          </Nav>
 
-          {/*collapsible navbar */}
-          <Navbar.Collapse id="basic-navbar-nav">
-
-            {/* navigation aligned to left*/}
-            <Nav className="me-auto">
-              <Nav.Link href="#">Home</Nav.Link>
-            </Nav>
-            
-            <Nav className='mr-auto'>
+          <Nav className='mr-auto'>
             <Form className="d-flex ms-auto">
-              <FormControl type="search" placeholder="Search" className="me-2" aria-label="Search" />
+              <FormControl type="search" placeholder="Enter PO Number" className="me-2" aria-label="Search" />
               <Button variant="primary">Search</Button>
             </Form>
             </Nav>
-          </Navbar.Collapse>
-        </header_Container>
-      </Navbar>
-      <header className="App-header">
+
+      </div>
+      
+      
+
+      <body className="App-header">
         <Container>
+
           <div className='customer-form'>
             <h2>Customer Order Placement Form</h2>
             <Form>
+              
               <Row className="mb-3">
                 <FormLabel column sm={2}>Customer Name:</FormLabel>
                 <Col sm={10}>
@@ -100,10 +93,13 @@ function App() {
                   <FormControl type='text' placeholder='Enter Order Status' />
                 </Col>
               </Row>
+              <button type="submit">Submit</button>
             </Form>
           </div>
+
+          
         </Container>
-      </header>
+      </body>
     </div>
   );
 }
